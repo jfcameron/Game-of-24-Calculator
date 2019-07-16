f7f5c9bd9d5bf1032949a8b63573435500f0394e
Joseph Cameron <jf.cameron@gmail.com>
2019-07-16 19:16:19 -0400

    wip

--- workspace/build_web.sh
+++ workspace/build_web.sh
@@ -7,12 +7,12 @@ PUBLIC_HEADER_DIR="../include"
 PRIVATE_HEADER_DIR="${SOURCE_ROOT_DIR}/include"
 
 #NOTE: c++1z not 17, https://github.com/emscripten-core/emscripten/issues/5513
-#    -o output.html \
 emcc \
     "${SOURCE_ROOT_DIR}"/*.cpp \
     -I"${PUBLIC_HEADER_DIR}" \
     -I"${PRIVATE_HEADER_DIR}" \
     -std=c++1z \
+    -o output.html \
     --pre-js "${WEBSRC_ROOT_DIR}/pre-js.js" \
     --post-js "${WEBSRC_ROOT_DIR}/post-js.js" \
     -DBUILD_WEB
